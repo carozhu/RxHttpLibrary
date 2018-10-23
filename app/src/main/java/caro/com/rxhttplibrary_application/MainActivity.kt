d71a3f2b638c7822e956291a4945a18cbad91ce4
carozhu <1025807062@qq.com>
2018-10-24 02:41:27 +0800

    remove Gson

--- app/src/main/java/caro/com/rxhttplibrary_application/MainActivity.kt
+++ app/src/main/java/caro/com/rxhttplibrary_application/MainActivity.kt
@@ -1,10 +1,10 @@
 package caro.com.rxhttplibrary_application
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,8 +16,6 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
         }
     }
 
